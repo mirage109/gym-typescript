2de53855a1676c104ca10088a41bc871005d489e
Anastasiia Zibla <zibla.asia@gmail.com>
2024-01-27 12:21:45 +0100

    feat: navbar mobile adoptation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [itTopOfPage, setItTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setItTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) {
+        setItTopOfPage(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div className="app bg-gray-20 ">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar isTopOfPage={itTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 }

8d27ecb17c7c0722ac5280879414d31ba3717b1c
Anastasiia Zibla <zibla.asia@gmail.com>
2024-01-28 20:08:56 +0100

    feat: benefitPage finished

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -90,7 +90,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
-        <div>
+        <div className="mt-16 item-center justify-between gap-20 md:mt-28 md:flex">
           <img
             className="mx-auto"
             alt="benefits-page-graphic"
@@ -99,15 +99,31 @@ const Benefits = ({ setSelectedPage }: Props) => {
           <div>
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
-                <div>
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}>
                   <HText>
                     MILLIONS OF HAPPY MEMBERS GETTING{" "}
                     <span className="text-primary-500">FIT</span>
                   </HText>
-                </div>
+                </motion.div>
               </div>
             </div>
-            <div>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay:0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}>
               <p className="my-5">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 Voluptatem, suscipit. Odit iusto doloribus suscipit quos
@@ -124,7 +140,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
                 dolor rem dicta animi distinctio accusantium dolorem, iusto
                 harum consequuntur fuga blanditiis corrupti explicabo?
               </p>
-            </div>
+            </motion.div>
             <div className="relative mt-16">
               <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
                 <ActionButton setSelectedPage={setSelectedPage}>
